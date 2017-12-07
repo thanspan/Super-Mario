c7ca5d2e049797044a499a79ea5debbb6cbec7df
THANASIS SPANOS <32618688+thanspan@users.noreply.github.com>
2017-12-07 17:58:29 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -18,7 +18,7 @@ var menu = {
 			
 			var menuimage = game.add.sprite(8, 8, 'menu');
 
-			var lvltxt = game.add.text(38, 150, 'lvl1', {fontSize: '12px', fill: '#000'});
+			var lvltxt = game.add.text(38, 150, 'level 1', {fontSize: '12px', fill: #FFFFFF'});
 			var click1 = game.add.button(60, 197, 'lvl1', function() {
 			game.state.start('lvl1');
 			menusong.stop();
