af960f54c9e678d736dd3a2e0e29579dab0f201c
THANASIS SPANOS <32618688+thanspan@users.noreply.github.com>
2017-12-07 23:02:02 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -19,7 +19,9 @@ var menu = {
 			
 			var menuimage = game.add.sprite(8, 8, 'menu');
                           
-			var lvltxt1 = game.add.text(30,200, 'Select level :', {fontSize: '13px', fill: '#fff'});
+			var lvltxt1 = game.add.text(30,20, 'Super', {fontSize: '17px', fill: '#000ff'});
+			var lvltxt1 = game.add.text(35,20, 'Mario', {fontSize: '17px', fill: '#00f'});
+			var lvltxt1 = game.add.text(33,30, 'Game', {fontSize: '17px', fill: '#ff0'});
 			var lvltxt = game.add.text(30, 150, 'Select level :', {fontSize: '12px', fill: '#fff'});
 			var click1 = game.add.button(60, 197, 'lvl1', function() {
 			game.state.start('lvl1');
