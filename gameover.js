60cbe89aca637077405e563cc4aeb9acd3479578
THANASIS SPANOS <32618688+thanspan@users.noreply.github.com>
2018-01-16 15:38:28 +0200

    Create gameover.js

---
+++ gameover.js
@@ -0,0 +1,22 @@
+var gameover = {
+
+	preload: function(){
+		game.load.audio('gameovs', 'audio/gameovsou.mp3');
+	},
+
+	create: function(){
+		gameovs = game.add.audio('gameovs');
+		gameovs.play();
+
+		game.stage.backgroundColor = '#000000';
+
+		var finalscoreText = game.add.text(80,150, 'Your score: ' + score, {fontSize: '10px', fill: '#fff'});
+
+		var gameovertxt = game.add.text(50,90, '!!GAME OVER!!', {fontSize: '25px', fill: '#fff'});
+    var gameovertxt = game.add.text(50,100, '!!TRY AGAIN!!', {fontSize: '25px', fill: '#fff'});
+		game.time.events.add(Phaser.Timer.SECOND * 5, function(){
+			score = 0;
+			game.state.start('Menu');
+		});
+	}
+}
