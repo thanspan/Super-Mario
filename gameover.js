02e7bbc49e68904a600693177d63b898c3d6875e
THANASIS SPANOS <32618688+thanspan@users.noreply.github.com>
2018-01-16 20:22:33 +0200

    Update gameover.js

--- gameover.js
+++ gameover.js
@@ -10,10 +10,10 @@ var gameover = {
 
 		game.stage.backgroundColor = '#000000';
 
-		var finalscoreText = game.add.text(80,150, 'Your score: ' + score, {fontSize: '10px', fill: '#fff'});
+		var finalscoreText = game.add.text(80,200, 'Your score: ' + score, {fontSize: '10px', fill: '#E00'});
 
 		var gameovertxt = game.add.text(50,90, '!!GAME OVER!!', {fontSize: '25px', fill: '#00f'});
-                var gameovertxt = game.add.text(50,200, '!!TRY AGAIN!!', {fontSize: '25px', fill: '#00f'});
+                var gameovertxt = game.add.text(50,150, '!!TRY AGAIN!!', {fontSize: '25px', fill: '#00f'});
 		game.time.events.add(Phaser.Timer.SECOND * 5, function(){
 			score = 0;
 			game.state.start('menu');
