0a796a75923eb831a163259594ceed9eb0aa39e3
THANASIS SPANOS <32618688+thanspan@users.noreply.github.com>
2018-01-18 22:18:39 +0200

    Update lvl2.js

--- lvl2.js
+++ lvl2.js
@@ -11,8 +11,8 @@ preload: function() {
 			
 			//We need this because the assets are on github pages
 			//Remove the next 2 lines if running locally
-			this.load.baseURL = ' https://thanspan.github.io/Super-Mario/';
-			this.load.crossOrigin = 'anonymous';
+			//this.load.baseURL = ' https://thanspan.github.io/Super-Mario/';
+			//this.load.crossOrigin = 'anonymous';
 		        this.load.image('live','assets/images.png');
 		        this.load.spritesheet('tiles', 'assets/super_mario_tiles1.png', 16,16);
 			this.load.spritesheet('goomba', 'assets/goomba1.png', 16, 16);
@@ -25,21 +25,23 @@ preload: function() {
 		        },
 			
 create : function(){
-                        musicbs = game.add.audio('music');
-		        musicbs.play();
+                        
+            musicbs = game.add.audio('music');
+		    musicbs.play();
+
 			Phaser.Canvas.setImageRenderingCrisp(game.canvas)
 			
-			game.stage.backgroundColor = 	'#5c94fc';
-                        pipesound = game.add.audio('pipesound');
+			game.stage.backgroundColor = 	'#cc66ff';
+            pipesound = game.add.audio('pipesound');
 			
 			map = game.add.tilemap('level');
-			map.addTilesetImage('tiles', 'tiles');
+			map.addTilesetImage('super_mario_tiles1', 'tiles');
 			map.setCollisionBetween(3, 12, true, 'solid');
 			//map.setCollisionBetween(3, 12, true, 'pipe');
 			map.createLayer('background');
            
 			layer = map.createLayer('solid');
-                        layer.resizeWorld();
+            layer.resizeWorld();
 			pipe = map.createLayer('pipe');
 			//pipe.resizeWorld();
 			
@@ -59,9 +61,9 @@ create : function(){
 			goombas.setAll('body.velocity.x', -20);
 			goombas.setAll('body.gravity.y', 500);
 	
-	                door = game.add.group();
-		        door.enableBody = true;
-		        map.createFromTiles(3, null, 'door', 'stuff', door);
+	        door = game.add.group();
+		    door.enableBody = true;
+		    map.createFromTiles(3, null, 'door', 'stuff', door);
 	
 			enemy = game.add.group();
 			enemy.enableBody = true;
@@ -71,27 +73,29 @@ create : function(){
 			enemy.setAll('body.bounce.x', 1);
 			enemy.setAll('body.velocity.x', -20);
 			enemy.setAll('body.gravity.y', 500);
-	game.physics.arcade.enable(player);
-	                player = game.add.sprite(16, game.world.height - 48, 'mario');
-			
+
+	        //game.physics.arcade.enable(player);
+	        player = game.add.sprite(16, game.world.height - 48, 'mario');
+			game.physics.arcade.enable(player);
 			player.body.gravity.y = 370;
 			player.body.collideWorldBounds = true;
 			player.animations.add('walkRight', [ 1, 2, 3 ], 10, true);
 			player.animations.add('walkLeft', [ 8, 9, 10 ], 10, true);
 			player.goesRight = true;
-			 
-	                 game.camera.follow(player);
-			 scoretxt = game.add.text(16, 16, 'Score: 0', { fontSize: '10px', fill: '#000' });
-                         scoretxt.fixedToCamera = true;
+
+			game.camera.follow(player);
+
+			scoretxt = game.add.text(16, 16, 'Score: 0', { fontSize: '10px', fill: '#000' });
+            scoretxt.fixedToCamera = true;
 			cursors = game.input.keyboard.createCursorKeys();
-                        livesimg=game.add.sprite(140,15,'live');
-                        livesco=game.add.text(158,20,'x '+lives,{font:'30px',fontSize: '10px',  fill:'black'});
+            livesimg=game.add.sprite(140,15,'live');
+            livesco=game.add.text(158,20,'x '+lives,{font:'30px',fontSize: '10px',  fill:'black'});
 			livesco.fixedToCamera=true;
 			livesimg.fixedToCamera=true;
 	
-	var style5 = { font: "bold 80px Roman", fill: "#4bb84b", boundsAlignH: "center", boundsAlignV: "middle" };
-		var	lvltext = game.add.text(280, 100, "CONGRATULATIONS", style5);
-		var	lvltext = game.add.text(280, 150, "NEXT STAGE", style5);
+	    var style5 = { font: "bold 20 Roman", fill: "#4bb84b", boundsAlignH: "center", boundsAlignV: "middle" };
+		var	lvltext = game.add.text(100, 60, "CONGRATULATIONS", style5);
+		var	lvltext = game.add.text(100, 80, "NEXT STAGE", style5);
 			
 			
 		},
@@ -103,10 +107,10 @@ create : function(){
 			game.physics.arcade.overlap(player, goombas, goombaOverlap);
 			game.physics.arcade.overlap(player, coins, coinOverlap);
 			game.physics.arcade.overlap(player,enemy, enemyOverlap);
-	                game.physics.arcade.collide(player,door,doorcollide);
-			 game.physics.arcade.enable(player);
+	        game.physics.arcade.collide(player,door,doorcollide);
+		    game.physics.arcade.enable(player);
 	  
-	                if (player.body.enable) {
+	        if (player.body.enable) {
 				player.body.velocity.x = 0;
 				if (cursors.left.isDown) {
 					player.body.velocity.x = -90;
