a912d8a62e98d3944a0a188ffd9b80fef86bfe8d
THANASIS SPANOS <32618688+thanspan@users.noreply.github.com>
2018-01-19 17:43:36 +0200

    Update lvl2.js

--- lvl2.js
+++ lvl2.js
@@ -104,7 +104,7 @@ create : function(){
 			game.physics.arcade.overlap(player, goombas, goombaOverlap);
 			game.physics.arcade.overlap(player, coins, coinOverlap);
 			game.physics.arcade.overlap(player,enemy, enemyOverlap);
-	        game.physics.arcade.collide(player,door,doorcollide);
+	        game.physics.arcade.collide(player,door2,door2collide);
 		    game.physics.arcade.enable(player);
 	  
 	        if (player.body.enable) {
